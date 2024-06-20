bc252682cff620a84da258754e0f45d1e81f7483
zeyad mohamed <zeyadelsharawy709@gmail.com>
2024-06-20 09:16:53 +0300

    ....

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+      <html lang="en">
         <body className={inter.className}>
           <QueryProvider>
             <SheetProvider />
