7fe84a19c58f682f86ce7031a0adc843c04ec235
zeyad mohamed <zeyadelsharawy709@gmail.com>
2024-06-20 08:44:47 +0300

    fix small bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import { Loader2 } from "lucide-react";
 import { WelcomeMsg } from "./welcome-msg";
 import { Filters } from "./filters";
 
-export const Header = () => {
+const Header = () => {
   return (
     <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-4 pb-36">
       <div className="max-w-screen-2xl mx-auto">
@@ -27,3 +27,5 @@ export const Header = () => {
     </header>
   );
 };
+
+export default Header;
